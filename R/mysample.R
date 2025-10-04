8cee4e6f189614e6704c4e87b7049f43611cf1fd
zachary-allen823 <zachary.s.allen@outlook.com>
2025-10-03 20:51:13 -0500

    added mysample function

---
+++ R/mysample.R
@@ -0,0 +1,27 @@
+#' mysample
+#'
+#' @param n a numeric
+#' @param iter a numeric
+#' @param time a numeric float value
+#'
+#' @returns a set of bargraphs
+#' @export
+#'
+#' @examples
+#' mysample(n=1000, iter=30, time=1)
+mysample=function(n, iter=10,time=0.5){
+  for( i in 1:iter){
+    #make a sample
+    s=sample(1:10,n,replace=TRUE)
+    # turn the sample into a factor
+    sf=factor(s,levels=1:10)
+    #make a barplot
+    barplot(table(sf)/n,beside=TRUE,col=rainbow(10),
+            main=paste("Example sample()", " iteration ", i, " n= ", n,sep="") ,
+            ylim=c(0,0.2)
+    )
+
+    #release the table
+    Sys.sleep(time)
+  }
+}
---
+++ tests/testthat/test-birthday.R
@@ -0,0 +1,3 @@
+test_that("the birthday function works", {
+  expect_gt(birthday(23), 0.40)
+})
