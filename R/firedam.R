8a9e48e2c3181a57ebf4ad607c34b5dc7aa4dc27
zachary-allen823 <zachary.s.allen@outlook.com>
2025-10-24 22:26:28 -0500

    added firedam data to package

---
+++ R/firedam.R
@@ -0,0 +1,14 @@
+#' FIREDAM.csv data
+#'
+#' A subset of data from the FIREDAM.csv data set ...
+#'
+#' @format ## `firedam`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{Distance}{Damage}
+#'   \item{num}{num}
+#'   \item{number}{number}
+#'   ...
+#' }
+#' @source <C:/RPACKAGES/MATH4753F25zachary/data/firedam.rda>
+"firedam"
